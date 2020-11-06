34906c70059255e68047e80f4d9535c82b51c33e
Alex Alejandro <eaglesalejandro.aa@gmail.com>
2020-11-06 14:09:27 -0500

    showdata brought in, seasons.pug in progress

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const app = express()
-const kimC = require('./kim')
+const showdata = require('./showdata')
 
 app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
-  res.render('index', { kimC })
+  return res.render('index', { showdata })
+})
+
+app.get('/season/:id', (req, res) => {
+  const season = showdata.seasons.find((season) => season.number === parseInt(req.params.id))
+
+  return res.render('seasons', { season, show: showdata.title })
 })
 
 app.all('*', (req, res) => {
@@ -13,3 +19,4 @@ app.all('*', (req, res) => {
 })
 app.listen(5000)
 console.log('Listening to Port 5000')
+
--- kim.js
+++
@@ -1,8 +0,0 @@
-const kimC = {
-  title: 'Kim\'s Convenience',
-  developers: ['Ins Choi', 'Kevin White'],
-  starring: ['Paul Sun-Hyung Lee', 'Jean Yoon', 'Andrea Bang', 'Simu Liu', 'Andrew Phung', 'Nicole Power'],
-  seasons: ['Season 1', 'Season 2', 'Season 3', 'Season 4']
-}
-
-module.exports = kimC
